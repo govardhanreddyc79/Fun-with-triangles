1624328eb428158f3c9580b41951f1b694399c0d
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-26 16:16:09 +0530

    added checkFormValues method

--- quiz.js
+++ quiz.js
@@ -5,17 +5,35 @@ var displayMessage = document.querySelector('#output-msg')
 
 const correctAnswers = ['90°', 'right angled', 'one right angle', '12, 16, 20','Equilateral triangle','100°','30°','a + b + c','no','45°']; 
 
+
+function checkFormValues(inputValues){
+    let index = 0;
+    for(let value of inputValues){
+        index = index + 1;
+    }
+    
+    if(index === correctAnswers.length){
+        return true;
+    } else {
+        alert("Please attempt the entire quiz.")
+    }
+}
+
 function calculateScore(){
     var score = 0
     var index = 0
     const data = new FormData(quizForm);
-    for(let value of data.values()){
-        if(value === correctAnswers[index]){
-            score += 1
+    if(checkFormValues(data.values()))
+    {
+        for(let value of data.values()){
+            console.log(value)
+            if(value === correctAnswers[index]){
+                score += 1
+            }
+            index += 1
         }
-        index += 1
-    }
-    showMessage("Your score is "+ score)
+        showMessage("Your score is "+ score)
+}
 }
 
 function showMessage(msg){
